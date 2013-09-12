872428b7cb8ca4ec16fd663228dc8787f43e3894
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2013-09-12 20:45:14 +0200

    Update HermodValidatorMojo.java

--- src/main/java/com/github/hermod/generator/HermodValidatorMojo.java
+++ src/main/java/com/github/hermod/generator/HermodValidatorMojo.java
@@ -14,47 +14,67 @@ import com.github.hermod.generator.impl.ClassContainerDescriptorValidator;
 import com.github.hermod.generator.model.ClassContainerDescriptor;
 
 /**
- * <p>HermodValidatorMojo. </p>
- *
+ * <p>
+ * HermodValidatorMojo.
+ * </p>
+ * 
  * @author anavarro - Mar 24, 2013
- *
+ * 
  */
-@Mojo(name = "validate", defaultPhase = LifecyclePhase.GENERATE_SOURCES ,
-requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME,
-requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME) // (since Maven 3.0))
-public class HermodValidatorMojo extends AbstractMojo {
-
+@Mojo(name = "validate", defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
+// (since Maven 3.0))
+public class HermodValidatorMojo
+    extends AbstractHermodMojo
+{
+    
     @Parameter(property = "modelType", defaultValue = "ANNOTATED_CLASSES")
     private String modelType;
-
+    
     @Parameter(property = "modelName", defaultValue = "com.github.hermod.ser.descriptor.AMessage")
     private String modelName;
     
     @Parameter(property = "packageToScan", defaultValue = "")
     private String packageToScan = "";
+    
     /**
      * (non-Javadoc)
      * 
      * @see org.apache.maven.plugin.Mojo#execute()
      */
     @Override
-    public void execute() throws MojoExecutionException {
+    public void execute() throws MojoExecutionException
+    {
+        if (skip)
+        {
+            getLog().info("HermodBasicGeneratorMojo skipped (see conf).");
+            return;
+        }
+
+        configurePluginClasspath();
         getLog().info("HermodValidator started.");
         final Parser classParser = new AnnotatedClassParser();
         final Validator<ClassContainerDescriptor> validator = new ClassContainerDescriptorValidator();
-
-        final ClassContainerDescriptor classContainerDescriptor = classParser.parse(this.modelName, this.packageToScan, "",
-                "", "", "genericName",
-                "");
+        
+        final ClassContainerDescriptor classContainerDescriptor = classParser.parse(this.modelName,
+                                                                                    this.packageToScan,
+                                                                                    "",
+                                                                                    "",
+                                                                                    "",
+                                                                                    "genericName",
+                                                                                    "");
         final List<String> errors = validator.validate(classContainerDescriptor);
-
-        if (errors.size() > 0) {
-             for (String error : errors) {
+        
+        if (errors.size() > 0)
+        {
+            for (String error : errors)
+            {
                 getLog().error(error);
             }
-            getLog().error("Model is invalid (see errors above).");
-        } else {
-            getLog().info("Model is valid.");
+            throw new MojoExecutionException("Model is invalid (see errors above).");
+        }
+        else
+        {
+            getLog().debug("Model is valid.");
         }
         getLog().info("HermodValidator ended.");
     }
