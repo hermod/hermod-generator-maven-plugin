8f93dca1023bee6bef5f69151bc2785e46ac0491
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2013-09-12 20:44:01 +0200

    Update HermodBasicGeneratorMojo.java

--- src/main/java/com/github/hermod/generator/HermodBasicGeneratorMojo.java
+++ src/main/java/com/github/hermod/generator/HermodBasicGeneratorMojo.java
@@ -1,9 +1,11 @@
 package com.github.hermod.generator;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -17,38 +19,41 @@ import com.github.hermod.generator.impl.MustacheGenerator;
 import com.github.hermod.generator.model.ClassContainerDescriptor;
 
 /**
- * <p>HermodGeneratorMojo. </p>
+ * <p>
+ * HermodGeneratorMojo.
+ * </p>
  * 
  * @author anavarro - Mar 20, 2013
  * 
  */
-@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES ,
-requiresDependencyResolution = ResolutionScope.COMPILE,
-requiresDependencyCollection = ResolutionScope.COMPILE) // (since Maven 3.0))
-public final class HermodBasicGeneratorMojo extends AbstractMojo {
-
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresDependencyResolution = ResolutionScope.COMPILE, requiresDependencyCollection = ResolutionScope.COMPILE)
+// (since Maven 3.0))
+public final class HermodBasicGeneratorMojo
+    extends AbstractHermodMojo
+{
+    
     // Parser options
     @Parameter(property = "modelType", defaultValue = "ANNOTATED_CLASSES")
-    private String modelType;
+    private String  modelType;
     
     @Parameter(property = "modelName", defaultValue = "com.github.hermod.ser.descriptor.AMessage")
-    private String modelName;
-
+    private String  modelName;
+    
     @Parameter(property = "packageToScan", defaultValue = "")
-    private String packageToScan = "";
+    private String  packageToScan = "";
     
     // Generator options
     @Parameter(property = "generateFileForEachClass", defaultValue = "false")
     private boolean generateFileForEachClass;
     
     @Parameter(property = "templateFileName", defaultValue = "messages-doc.xdoc.mustache")
-    private String templateFileName; // templateName.extension.mustache don't generate is blank
-    
+    private File  templateFileName;                // templateName.extension.mustache don't generate is blank
+                                                      
     @Parameter(property = "generatePackageLayoutInOutputDir", defaultValue = "false")
     private boolean generatePackageLayoutInOutputDir;
     
     @Parameter(property = "outputDir", defaultValue = "${project.build.directory}/generated-site")
-    private String outputDir;
+    private File  outputDir;
     
     /**
      * (non-Javadoc)
@@ -56,28 +61,82 @@ public final class HermodBasicGeneratorMojo extends AbstractMojo {
      * @see org.apache.maven.plugin.Mojo#execute()
      */
     @Override
-    public void execute() throws MojoExecutionException {
+    public void execute() throws MojoExecutionException
+    {
+        if (skip)
+        {
+            getLog().info("HermodBasicGeneratorMojo skipped (see conf).");
+            return;
+        }
+            
         getLog().info("HermodBasicGeneratorMojo started.");
         final Parser classParser = new AnnotatedClassParser();
         final Validator<ClassContainerDescriptor> validator = new ClassContainerDescriptorValidator();
         final Generator generator = new MustacheGenerator();
+        
+        configurePluginClasspath();
+        outputDir.mkdirs();
+        if (!outputDir.exists())
+            throw new MojoExecutionException("Impossible to create Output dir: {}"+outputDir);
+        
+        final File templateFile = templateFileName;
+        if (!templateFile.exists())
+            throw new MojoExecutionException("Template file doesn't exist: "+templateFile.getAbsolutePath());
+        
+        final Matcher match = scan(templateFile.getName());
+        if (!match.matches())
+            throw new MojoExecutionException("template file doesn't match the patter: '(.*).extension.mustache' : "+templateFile.getName());
 
-        final String normalizedName = this.templateFileName.contains(File.separator) ? StringUtils.substringAfterLast(StringUtils.substringBefore(this.templateFileName, "."), File.separator) : StringUtils.substringBefore(this.templateFileName, ".");
-        final ClassContainerDescriptor classContainerDescriptor = classParser.parse(this.modelName, this.packageToScan, "",
-                "", "", normalizedName,
-                "");
+        final String normalizedName = match.group(1);
+        
+        //        final String normalizedName = this.templateFileName.contains(File.separator) 
+        //                ? StringUtils.substringAfterLast(StringUtils.substringBeforeLast(this.templateFileName, "."), File.separator) 
+        //                : StringUtils.substringBeforeLast(this.templateFileName, ".");
+
+                
+                final ClassContainerDescriptor classContainerDescriptor = classParser.parse(this.modelName,
+                                                                                    this.packageToScan,
+                                                                                    "",
+                                                                                    "",
+                                                                                    "",
+                                                                                    normalizedName,
+                                                                                    "");
+        
+        getLog().debug("normalizedName: " + normalizedName);
         final List<String> errors = validator.validate(classContainerDescriptor);
-
-        if (errors.size() > 0) {
-            for (String error : errors) {
+        
+        if (errors.size() > 0)
+        {
+            for (String error : errors)
+            {
                 getLog().error(error);
             }
             getLog().error("Model is invalid (see errors above).");
             throw new IllegalArgumentException("Model is invalid");
-        } else {
-                generator.generateSourceClasses(classContainerDescriptor, this.outputDir, this.generatePackageLayoutInOutputDir,
-                        this.templateFileName, this.generateFileForEachClass, ClassType.BASIC);
+        }
+        else
+        {
+            try
+            {
+                generator.generateSourceClasses(classContainerDescriptor,
+                                                this.outputDir,
+                                                this.generatePackageLayoutInOutputDir,
+                                                this.templateFileName,
+                                                this.generateFileForEachClass,
+                                                ClassType.BASIC);
+            }
+            catch (IOException aException)
+            {
+                throw new MojoExecutionException("Generate Failed", aException);
+            }
         }
         getLog().info("HermodBasicGeneratorMojo ended.");
     }
+    
+    private static Matcher scan(String aText)
+    {
+        final Pattern p = Pattern.compile("(.*)\\.(.*)\\.(mustache)$");
+        return p.matcher(aText);
+    }
+
 }
