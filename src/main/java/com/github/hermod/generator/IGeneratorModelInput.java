c1189d24136239668daa13d07cb8079444ff521e
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2013-09-12 20:45:39 +0200

    Create IGeneratorModelInput.java

---
+++ src/main/java/com/github/hermod/generator/IGeneratorModelInput.java
@@ -0,0 +1,15 @@
+package com.github.hermod.generator;
+
+import java.util.Map;
+
+/**
+ * IGeneratorModelInput.
+ *
+ * @author clalleme - May 3, 2013
+ *
+ * <!-- $Id: PreTradeEclipseCodeTemplatesRules.xml,v 1.4 2009/11/26 23:27:51 clalleme Exp $ -->.
+ */
+public interface IGeneratorModelInput
+{
+    public Map<String, Object> getModel();
+}
