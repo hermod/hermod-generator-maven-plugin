7ea26cfe9e86701fe42a4d93c068a3d1131c698f
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2013-09-12 20:48:09 +0200

    Update ClassDescriptor.java

--- src/main/java/com/github/hermod/generator/model/ClassDescriptor.java
+++ src/main/java/com/github/hermod/generator/model/ClassDescriptor.java
@@ -1,38 +1,50 @@
 package com.github.hermod.generator.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.github.hermod.generator.ClassType;
-import com.google.common.base.Preconditions;
+import com.github.hermod.ser.descriptor.AMessage;
+import com.github.hermod.ser.descriptor.ESender;
 import com.google.common.collect.Maps;
 
 /**
- * <p>MessageDescriptor. </p>
+ * <p>
+ * MessageDescriptor.
+ * </p>
  * 
  * @author anavarro - Mar 10, 2013
  * 
  */
-public class ClassDescriptor {
-
-    private final String name;
-    private final String packageName;
-    private final int id;
-    private final String docName;
-    private final String docComment;
-    private final String prefixImplementationName;
-    private final String prefixInterfaceName;
-    private final String serializableImplementationClass;
-    private final String suffixImplementationPackageName;
-
-    private final Collection<FieldDescriptor> fields;
+public class ClassDescriptor
+{
+    
+    private final String                       name;
+    private final String                       javaName;
+    private final String                       packageName;
+    private final int                          id;
+    private final String                       docName;
+    private final String                       docComment;
+    private final String                       prefixImplementationName;
+    private final String                       prefixInterfaceName;
+    private final String                       serializableImplementationClass;
+    private final String                       suffixImplementationPackageName;
+    private boolean                            isContainsUuid;
+    private boolean                            isContainsIMsg;
+    
+    private final List<FieldDescriptor>        fields;
+    private final List<EnumDescriptor>         enums;
     private final Collection<MethodDescriptor> methods;
-    private final Collection<ClassDescriptor> responseMessages;
-
+    private final Collection<AMessage>         responseMessages;
+    private final Collection<ESender>          sentBy;
+    
     /**
      * Constructor.
      * 
@@ -40,14 +52,21 @@ public class ClassDescriptor {
      * @param aPackageName
      * @param aId
      * @param aDocName
-     * @param aDocComment TODO
-     * @param aSuffixImplementationPackageName TODO
+     * @param aDocComment
+     *            TODO
+     * @param aSuffixImplementationPackageName
+     *            TODO
      * @param aFields
+     * @param aESenders 
      */
-    public ClassDescriptor(final String aName, final String aPackageName, final int aId, final String aDocName,
-            final String aDocComment, final String aPrefixImplementationName, final String aPrefixInterfaceName,
-            final String aSuffixImplementationPackageName, final String aserializableImplementationClass, final Collection<FieldDescriptor> aFields, final Collection<MethodDescriptor> aMethods, final Collection<ClassDescriptor> aResponseMessages) {
+    public ClassDescriptor(final String aJavaName, final String aName, final String aPackageName, final int aId, final String aDocName,
+                           final String aDocComment, final String aPrefixImplementationName, final String aPrefixInterfaceName,
+                           final String aSuffixImplementationPackageName, final String aserializableImplementationClass,
+                           final List<FieldDescriptor> aFields, final Collection<MethodDescriptor> aMethods,
+                           final Collection<AMessage> aResponseMessages, List<EnumDescriptor> aEnums, ESender[] aESenders)
+    {
         super();
+        this.javaName = aJavaName;
         this.name = aName;
         this.packageName = aPackageName;
         this.id = aId;
@@ -58,291 +77,382 @@ public class ClassDescriptor {
         this.serializableImplementationClass = aserializableImplementationClass;
         this.suffixImplementationPackageName = aSuffixImplementationPackageName;
         this.fields = aFields;
+        this.sentBy = Arrays.asList(aESenders);
+        Collections.sort(this.fields, new Comparator<FieldDescriptor>()
+        {
+            @Override
+            public int compare(FieldDescriptor aO1, FieldDescriptor aO2)
+            {
+                return aO1.getId() - aO2.getId();
+            }
+        });
+        this.enums = aEnums;
         this.methods = aMethods;
         this.responseMessages = aResponseMessages;
-        //validate();
-
+        this.isContainsIMsg = false;
+        this.isContainsUuid = false;
+        // validate();
+        
     }
-
+    
     /**
      * validateFields.
      * 
      * @param aFields
      * @throws IllegalArgumentException
      */
-    public List<String> validate() /*throws IllegalArgumentException*/ {
+    public List<String> validate() /* throws IllegalArgumentException */
+    {
         final List<String> errors = new ArrayList<>();
-        for (final FieldDescriptor fieldDescriptor : this.fields) {
-//            Preconditions.checkArgument(fieldDescriptor.getId() >= 0, "The field Id is %s but must >= 0 for message %s", fieldDescriptor.getId(),
-//                    this.getImplementationName());
-            if (fieldDescriptor.getId() < 0) {
-                errors.add(String.format("The field Id is %s but must >= 0 for message %s", fieldDescriptor.getId(),
-                        this.getImplementationName()));
+        for (final FieldDescriptor fieldDescriptor : this.fields)
+        {
+            // Preconditions.checkArgument(fieldDescriptor.getId() >= 0, "The field Id is %s but must >= 0 for message %s",
+            // fieldDescriptor.getId(),
+            // this.getImplementationName());
+            if (fieldDescriptor.getId() < 0)
+            {
+                errors.add(String.format("The field Id is %s but must >= 0 for message %s",
+                                         fieldDescriptor.getId(),
+                                         this.getImplementationName()));
             }
-//            Preconditions.checkArgument(fieldDescriptor.getName().matches("^[a-zA-Z][a-zA-Z0-9]*?$"),
-//                    "The field Name is %s but must match ^[a-zA-Z][a-zA-Z0-9]*?$ for message %s", fieldDescriptor.getName(),
-//                    this.getImplementationName());
-            if (!fieldDescriptor.getName().matches("^[a-zA-Z][a-zA-Z0-9]*?$")) {
-                errors.add(String.format("The field Name is %s but must match ^[a-zA-Z][a-zA-Z0-9]*?$ for message %s", fieldDescriptor.getName(),
-                        this.getImplementationName()));
+            // Preconditions.checkArgument(fieldDescriptor.getName().matches("^[a-zA-Z][a-zA-Z0-9]*?$"),
+            // "The field Name is %s but must match ^[a-zA-Z][a-zA-Z0-9]*?$ for message %s", fieldDescriptor.getName(),
+            // this.getImplementationName());
+            if (!fieldDescriptor.getName().matches("^[a-zA-Z][a-zA-Z0-9]*?$"))
+            {
+                errors.add(String.format("The field Name is %s but must match ^[a-zA-Z][a-zA-Z0-9]*?$ for message %s",
+                                         fieldDescriptor.getName(),
+                                         this.getImplementationName()));
             }
         }
-
+        
         final Map<Integer, FieldDescriptor> idMap = Maps.newHashMap();
-        for (final FieldDescriptor fieldDescriptor : this.fields) {
-//            Preconditions.checkArgument(!idMap.containsKey(fieldDescriptor.getId()),
-//                    "The field Id is %s for field %s is already used for field %s, it must be unique for message %s", fieldDescriptor.getId(),
-//                    fieldDescriptor.getName(), idMap.get(fieldDescriptor.getId()) != null ? idMap.get(fieldDescriptor.getId()).getName() : "",
-//                    this.name);
-            if (idMap.containsKey(fieldDescriptor.getId())) {
-                errors.add(String.format("The field Id is %s for field %s is already used for field %s, it must be unique for message %s", fieldDescriptor.getId(),
-                        fieldDescriptor.getName(), idMap.get(fieldDescriptor.getId()) != null ? idMap.get(fieldDescriptor.getId()).getName() : "",
-                                this.name));
+        for (final FieldDescriptor fieldDescriptor : this.fields)
+        {
+            // Preconditions.checkArgument(!idMap.containsKey(fieldDescriptor.getId()),
+            // "The field Id is %s for field %s is already used for field %s, it must be unique for message %s", fieldDescriptor.getId(),
+            // fieldDescriptor.getName(), idMap.get(fieldDescriptor.getId()) != null ? idMap.get(fieldDescriptor.getId()).getName() : "",
+            // this.name);
+            if (idMap.containsKey(fieldDescriptor.getId()))
+            {
+                errors.add(String.format("The field Id is %s for field %s is already used for field %s, it must be unique for message %s",
+                                         fieldDescriptor.getId(),
+                                         fieldDescriptor.getName(),
+                                         idMap.get(fieldDescriptor.getId()) != null ? idMap.get(fieldDescriptor.getId()).getName() : "",
+                                         this.name));
             }
             
             idMap.put(fieldDescriptor.getId(), fieldDescriptor);
             
             errors.addAll(fieldDescriptor.validate());
+            
+            if (fieldDescriptor.isUuid())
+                isContainsUuid = true;
+            if (fieldDescriptor.isMessage())
+                isContainsIMsg = true;
         }
         
         return errors;
     }
-
+    
     /**
      * getName.
      * 
      * @return
      */
-    public String getImplementationName() {
+    public String getImplementationName()
+    {
         return this.getPrefixImplementationName() + this.getName();
     }
-
+    
     /**
      * getName.
      * 
      * @param classType
      * @return
      */
-    public String getName(final ClassType classType) {
-        switch (classType) {
-        case INTERFACE:
-            return getInterfaceName();
-        case CLASS:
-            return getImplementationName();
-        case BASIC:
-            return getName();
-        default:
-            return getName();
+    public String getName(final ClassType classType)
+    {
+        switch (classType)
+        {
+            case INTERFACE:
+                return getInterfaceName();
+            case CLASS:
+                return getImplementationName();
+            case BASIC:
+                return getName();
+            default:
+                return getName();
         }
     }
-
+    
     /**
      * getInterfaceName.
      * 
      * @return
      */
-    public String getInterfaceName() {
+    public String getInterfaceName()
+    {
         return this.getPrefixInterfaceName() + this.getName();
     }
-
-    public String getCanonicalInterfaceName() {
+    
+    public String getCanonicalInterfaceName()
+    {
         return this.getPackageName() + "." + this.getInterfaceName();
     }
-
+    
     /**
      * getCapitalizeName.
      * 
      * @return
      */
-    public String getCapitalizeName() {
+    public String getCapitalizeName()
+    {
         return StringUtils.capitalize(this.getImplementationName());
     }
-
+    
     /**
      * getUpperInterfaceName.
      * 
      * @return
      */
-    public String getUpperInterfaceName() {
+    public String getUpperInterfaceName()
+    {
         return this.getInterfaceName().toUpperCase();
     }
-
+    
     /**
      * getOriginalName.
      * 
      * @return
      */
-    public String getName() {
+    public String getName()
+    {
         return this.name;
     }
-
+    
     /**
      * getUpperOriginalName.
      * 
      * @return
      */
-    public String getUpperName() {
+    public String getUpperName()
+    {
         return this.getName().toUpperCase();
     }
-
-
+    
     /**
      * getPackageName.
      * 
      * @return
      */
-    public String getImplementationPackageName() {
-        return this.getPackageName() + ("".equals(this.getSuffixImplementationPackageName()) ? "" : "." + this.getSuffixImplementationPackageName());
+    public String getImplementationPackageName()
+    {
+        return this.getPackageName()
+               + ("".equals(this.getSuffixImplementationPackageName()) ? "" : "." + this.getSuffixImplementationPackageName());
     }
     
-    public String getInterfacePackageName() {
+    public String getInterfacePackageName()
+    {
         return this.getPackageName();
     }
-
+    
     /**
      * getPrefixName.
      * 
      * @return
      */
-    public String getPrefixName() {
+    public String getPrefixName()
+    {
         return this.prefixImplementationName;
     }
-
+    
     /**
      * getSuffixPackageName.
      * 
      * @return
      */
-    public String getSuffixImplementationPackageName() {
+    public String getSuffixImplementationPackageName()
+    {
         return this.suffixImplementationPackageName;
     }
-
+    
     /**
      * getOriginalPackageName.
      * 
      * @return
      */
-    public String getPackageName() {
+    public String getPackageName()
+    {
         return this.packageName;
     }
-
+    
     /**
      * getPackageName.
      * 
      * @param classType
      * @return
      */
-    public String getPackageName(final ClassType classType) {
-        switch (classType) {
-        case INTERFACE:
-            return getInterfacePackageName();
-        case CLASS:
-            return getImplementationPackageName();
-        default:
-            return getImplementationPackageName();
+    public String getPackageName(final ClassType classType)
+    {
+        switch (classType)
+        {
+            case INTERFACE:
+                return getInterfacePackageName();
+            case CLASS:
+                return getImplementationPackageName();
+            default:
+                return getImplementationPackageName();
         }
     }
-
+    
     /**
      * getId.
      * 
      * @return
      */
-    public int getId() {
+    public int getId()
+    {
         return this.id;
     }
-
+    
     /**
      * getDocName.
      * 
      * @return
      */
-    public String getDocName() {
+    public String getDocName()
+    {
         return this.docName;
     }
     
-    
-
-    public String getDocComment() {
+    public String getDocComment()
+    {
         return this.docComment;
     }
-
+    
     /**
      * getFields.
      * 
      * @return
      */
-    public Collection<FieldDescriptor> getFields() {
+    public List<FieldDescriptor> getFields()
+    {
         return this.fields;
     }
-
+    
+    /**
+     * Returns the enums.
+     * 
+     * @return The enums to return.
+     */
+    public List<EnumDescriptor> getEnums()
+    {
+        return enums;
+    }
+    
     /**
      * getPrefix.
      * 
      * @return
      */
-    public String getPrefixImplementationName() {
+    public String getPrefixImplementationName()
+    {
         return this.prefixImplementationName;
     }
-
+    
     /**
      * getSuffix.
      * 
      * @return
      */
-    public String getPrefixInterfaceName() {
+    public String getPrefixInterfaceName()
+    {
         return this.prefixInterfaceName;
     }
-
+    
     /**
      * getImplementationClass.
      * 
      * @return
      */
-    public String getImplementationClass() {
+    public String getImplementationClass()
+    {
         return this.serializableImplementationClass;
     }
-
+    
     /**
      * getMethods.
      * 
      * @return
      */
-    public Collection<MethodDescriptor> getMethods() {
+    public Collection<MethodDescriptor> getMethods()
+    {
         return this.methods;
     }
     
-    
-
     /**
      * getResponseMessages.
-     *
+     * 
      * @return
      */
-    public Collection<ClassDescriptor> getResponseMessages() {
+    public Collection<AMessage> getResponseMessages()
+    {
         return this.responseMessages;
     }
-
+    
+    /**
+     * Returns the isContainsIMsg.
+     * 
+     * @return The isContainsIMsg to return.
+     */
+    public boolean isContainsIMsg()
+    {
+        return isContainsIMsg;
+    }
+    
+    /**
+     * Returns the isContainsUuid.
+     * 
+     * @return The isContainsUuid to return.
+     */
+    public boolean isContainsUuid()
+    {
+        return isContainsUuid;
+    }
+    
+    /**
+     * Returns the sentBy.
+     *
+     * @return The sentBy to return.
+     */
+    public Collection<ESender> getSentBy()
+    {
+        return sentBy;
+    }
+    
     /**
      * (non-Javadoc)
      * 
      * @see java.lang.Object#hashCode()
      */
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         final int prime = 31;
         int result = 1;
         result = prime * result + this.id;
         return result;
     }
-
+    
     /**
      * (non-Javadoc)
      * 
      * @see java.lang.Object#equals(java.lang.Object)
      */
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(Object obj)
+    {
         if (this == obj)
             return true;
         if (obj == null)
@@ -354,18 +464,29 @@ public class ClassDescriptor {
             return false;
         return true;
     }
-
+    
+    /**
+     * Returns the javaName.
+     * 
+     * @return The javaName to return.
+     */
+    public String getJavaName()
+    {
+        return javaName;
+    }
+    
     /**
      * (non-Javadoc)
      * 
      * @see java.lang.Object#toString()
      */
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "ClassDescriptor [name=" + this.name + ", packageName=" + this.packageName + ", id=" + this.id + ", docName=" + this.docName
-                + ", prefixImplementationName=" + this.prefixImplementationName + ", prefixInterfaceName=" + this.prefixInterfaceName
-                + ", implementationClass=" + this.serializableImplementationClass + ", suffixImplementationPackageName="
-                + this.suffixImplementationPackageName + ", fields=" + this.fields + ", methods=" + this.methods + "]";
+               + ", prefixImplementationName=" + this.prefixImplementationName + ", prefixInterfaceName=" + this.prefixInterfaceName
+               + ", implementationClass=" + this.serializableImplementationClass + ", suffixImplementationPackageName="
+               + this.suffixImplementationPackageName + ", fields=" + this.fields + ", methods=" + this.methods + "]";
     }
-
+    
 }
